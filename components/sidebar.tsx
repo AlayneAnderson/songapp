a4ef705ddf7a404991448ed02ca7cde560171789
Alayne Anderson <55996322+AlayneAnderson@users.noreply.github.com>
2022-04-13 22:31:42 -0700

    Prisma added

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -49,6 +49,8 @@ const musicMenu = [
     }
 ]
 
+const playlists = new Array(30).fill(1).map((_, i) => 'Playlist ${i + 1}')
+
 const Sidebar = () => {
     return (
         <Box 
@@ -58,7 +60,7 @@ const Sidebar = () => {
             paddingX="5px" 
             color="gray"
         >
-            <Box paddingY="20px">
+            <Box paddingY="20px" height="100%">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <NextImage src="/logo.svg" height={60} width={120}/>
                 </Box>
@@ -82,9 +84,6 @@ const Sidebar = () => {
                         ))}
                     </List>
                 </Box>
-                <Box>
-                    <Divider bg="gray.800" />
-                </Box>
                 <Box marginTop ="20px">
                     <List spacing={2}>
                         {musicMenu.map(menu => (
@@ -99,6 +98,22 @@ const Sidebar = () => {
                                             />
                                             {menu.name}
                                         </LinkOverlay>
+                                    </NextLink>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Box marginTop="20px">
+                </Box>
+                <Divider bg="gray.800" />
+                <Box height="66%" overflowY="auto" paddingY="20px">
+                    <List spacing={2}>
+                        {playlists.map(playlist =>(
+                            <ListItem paddingX="20px" key={playlist}>
+                                <LinkBox>
+                                    <NextLink href="/" passHref>
+                                        <LinkOverlay>{playlist}</LinkOverlay>
                                     </NextLink>
                                 </LinkBox>
                             </ListItem>
