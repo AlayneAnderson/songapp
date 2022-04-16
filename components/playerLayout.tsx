ac0eaab271a6de724af80f15eaf047b80d6e59dc
Alayne Anderson <55996322+AlayneAnderson@users.noreply.github.com>
2022-04-15 19:04:24 -0700

    init

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -9,7 +9,7 @@
                <Sidebar />
            </Box>
            <Box marginLeft="250px" marginBottom="100px">
-               <Box height ="calc(100vh -100px)">{children}</Box>
+               <Box height ="calc(100vh - 100px)">{children}</Box>
            </Box>
            <Box position="absolute" left="0" bottom="0">
                player
