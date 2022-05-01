44a65abc552c5e9a923e5ec9c03066f32d194f0b
Alayne Anderson <55996322+AlayneAnderson@users.noreply.github.com>
2022-04-30 23:01:24 -0700

    UI INIT

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
-const Home = () => {
-  return <div>home</div>
+const Home = (props) => {
+  return (<GradientLayout 
+    color = "red" 
+    roundImage 
+    subtitle="Profile" 
+    title = "Alayne Anderson" 
+    description="15 public playlists"
+    image ="IMG_2660.jpeg">
+      <div>home page</div>
+  </GradientLayout>)
 }
 
 export default Home
