24677fb15ade0e698a42b1b440dec6df9d7e98ab
Alayne Anderson <55996322+AlayneAnderson@users.noreply.github.com>
2022-05-01 21:26:21 -0700

    migration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
-import { Box, Flex, Text, Image } from '@chakra-ui/react'
-import Head from 'next/head'
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
+import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
-import styles from '../styles/Home.module.css'
 
-const Home = ({artists}) => {
-  return (<GradientLayout 
-    color = "red" 
-    roundImage 
-    subtitle="Profile" 
-    title = "Alayne Anderson" 
-    description="15 public playlists"
-    image ="IMG_2660.jpeg"
-  >
-    <Box color="white" paddingX="40px">
+const Home = ({ artists }) => {
+  const { user } = useMe()
+
+  return (
+    <GradientLayout
+      roundImage
+      color="red"
+      subtitle="profile"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
+      image="IMG_2660.jpeg"
+    >
+      <Box color="white" paddingX="40px">
         <Box marginBottom="40px">
           <Text fontSize="2xl" fontWeight="bold">
             Top artist this month
@@ -25,7 +28,7 @@ const Home = ({artists}) => {
             <Box paddingX="10px" width="20%">
               <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
                 <Image
-                  src="https://placekitten.com/300/300"
+                  src="IMG_3237.JPG"
                   borderRadius="100%"
                 />
                 <Box marginTop="20px">
@@ -37,15 +40,16 @@ const Home = ({artists}) => {
           ))}
         </Flex>
       </Box>
-  </GradientLayout>)
+    </GradientLayout>
+  )
 }
 
-//This function wont show on the client side. Next.JS would strip it out
 export const getServerSideProps = async () => {
   const artists = await prisma.artist.findMany({})
-  console.log(artists)
+
   return {
-    props: {artists},
+    props: { artists },
   }
 }
+
 export default Home
